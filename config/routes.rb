0b92206cb3af6607011369a715cb3aaefb7af7f8
Tom Horan <tomhoran91@gmail.com>
2020-12-14 16:39:27 +0100

    Smartlock challenge completed using solution vid

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  post '/handle_report', to: 'reports#handle', defaults: { format: :json }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
